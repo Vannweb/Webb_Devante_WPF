7805b70703e75c515ecd2e9eebc22eacaa396f9c
Vannweb <dawebb@fullsail.edu>
2014-12-05 15:27:28 -0500

    Pizza Slices
    Pizza slices assignment part 1 and 2

--- Expressions_WorkSheet/js/script2.js
+++ Expressions_WorkSheet/js/script2.js
@@ -1,2 +1,7 @@
 // Devante Webb Friday, Dec 5th 2014 Slice of Pie Part 1
+var pizzaCount = 5; // the amount of Pizzas ordered
+var pizzaSlices = pizzaCount * 6; // the amount of slices each pizza contains.
+var partyPeople = 8; // The amount of people at the party
 
+var peoplesPizza = pizzaSlices/partyPeople; //The result of the amount of slices each person is eating.
+console.log("Each person ate " + peoplesPizza + " slices of pizza."); // Print out how much pizza each person is eating
--- Expressions_WorkSheet/js/script3.js
+++ Expressions_WorkSheet/js/script3.js
@@ -1,3 +1,10 @@
-/**
- * Created by VanWeb on 12/5/14.
- */
+// Devante Webb Friday, Dec 5th 2014 Slice of Pie Part 2
+var pizzaCount = 5; // the amount of Pizzas ordered
+var pizzaSlices = pizzaCount * 8; // the amount of slices each pizza contains.
+var partyPeople = 9; // The amount of people at the party
+
+
+var dogPizza = pizzaSlices % partyPeople; // pizza slices the dog gets.
+var peoplesPizza = pizzaSlices/partyPeople; //The result of the amount of slices each person is eating.
+// Print out how much pizza the dog gets
+console.log("Spark gets " + dogPizza + " slices of pizza." );
