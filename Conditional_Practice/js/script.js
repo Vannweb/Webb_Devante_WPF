e8552c6f2d754eeef3927cd368a973c55110f2f3
Vannweb <dawebb@fullsail.edu>
2014-12-11 14:36:41 -0500

    Conditionals
    Practice with conditionals.

---
+++ Conditional_Practice/js/script.js
@@ -0,0 +1,10 @@
+//Conditional Logic
+
+var oldEnough = false;
+
+// if the child is old enough print to the console you can ride!
+if(oldEnough){
+    console.log("You can ride the coaster!");
+}
+// Code performed if the statement is true
+console.log("What comes after");
