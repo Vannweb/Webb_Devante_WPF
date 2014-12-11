4d2a4acfe706f6664d425f5034036132a145639e
Vannweb <dawebb@fullsail.edu>
2014-12-11 14:43:06 -0500

    Sneaker Lift
    conditionals and expressions

--- Conditional_Practice/js/script.js
+++ Conditional_Practice/js/script.js
@@ -1,10 +1,10 @@
 //Conditional Logic
 
-var oldEnough = false;
-
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
 // if the child is old enough print to the console you can ride!
-if(oldEnough){
+// if the kid is over 48 inches in height
+if(kidHeight + sneakerLift > minHeight){
     console.log("You can ride the coaster!");
 }
-// Code performed if the statement is true
-console.log("What comes after");
