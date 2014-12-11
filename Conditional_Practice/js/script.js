f9eb2c923a954326e03ff3ec336e3999b958cac8
Vannweb <dawebb@fullsail.edu>
2014-12-11 18:02:24 -0500

    parent height
    parent height

--- Conditional_Practice/js/script.js
+++ Conditional_Practice/js/script.js
@@ -1,12 +1,15 @@
 //Conditional Logic
 
-var kidHeight = 48;
+var kidHeight = 52;
 var minHeight = 48;
+var parentHeight = 45; // The height of the kid if they need a parent
 
 // if the child is old enough print to the console you can ride!
 // if the kid is over 48 inches in height
 if(kidHeight > minHeight){
     console.log("You can ride the coaster!");
+}else if(kidHeight > parentHeight){
+    console.log("You can ride, but only with a parent present.");
 }else{
-    console.log("Sorry kid, you've got some growing to do!");
+    console.log("Sorry kid, you've got some growing to do first!")
 }
