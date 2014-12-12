27b88464c9c27fddead638a84664e36046270f3c
Vannweb <dawebb@fullsail.edu>
2014-12-11 21:39:26 -0500

    Conditionals Personal
    added variables

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,1 +1,6 @@
 // Devante Webb 12/11/2014 Conditionals Personal, Memory Card Capacity
+
+var cardCap = Number(prompt("How much memory does your memory card have?"));
+var pictureNum = Number(prompt("How many pictures have you taken today?"));
+var cameraType = prompt("Is your camera a DLSR shooter(Y/N");
+
