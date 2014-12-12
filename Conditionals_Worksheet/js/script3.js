61ef629279d4b6bacda5b2660418f0b16b59bbf7
Vannweb <dawebb@fullsail.edu>
2014-12-11 20:27:00 -0500

    Conditionals Worksheet
    movie time and price( This one was actually pretty fun)

--- Conditionals_Worksheet/js/script3.js
+++ Conditionals_Worksheet/js/script3.js
@@ -1,1 +1,15 @@
-alert("This is in the js file");
+//Devante Webb 12/11/2014 Group 3: Movie Ticket Discounts
+
+//Prompts the user for their age
+var age = Number(prompt("Please enter your age:"));
+//prompts the user for the time
+var time = Number(prompt("Please enter the time you're going to see the movie(in whole numbers please): "));
+/* if the age of the person is below 10 OR 55 and above OR the time is greater than or equal to 3 and less than or equal
+ 5 then your ticket price will come up as 7*/
+if(age >= 55 || age < 10 || time >= 3 && time <= 5 ){
+    alert("Your ticket is 7 dollars");
+    console.log("Your ticket is 7 dollars");
+}else{ //else the price will come up as 12
+    alert("Your ticket is 12 dollars");
+    console.log("Your ticket is 12 dollars");
+}
