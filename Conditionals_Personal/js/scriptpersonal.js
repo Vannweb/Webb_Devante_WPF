f04df81476a17ff71b77ffafdd00c6fb7211bcd6
Vannweb <dawebb@fullsail.edu>
2014-12-11 22:23:04 -0500

    Conditionals Personal
    finished adding comments

--- Conditionals_Personal/js/scriptpersonal.js
+++ Conditionals_Personal/js/scriptpersonal.js
@@ -8,20 +8,20 @@ var cameraType = prompt("Is your camera a DLSR shooter(Y/N)");
 // if the camera is a DLSR we go through this block
 if(cameraType == "Y"){
     var raw = prompt("Did you shoot in Raw or JPEG(enter Raw or JPEG)"); // asks if you shot in raw or jpeg
-    if(raw == "Raw"){
+    if(raw == "Raw"){ //if Raw is selected the number of picture are multiplied by 20 because of bigger file size
          picSize = pictureNum * 24;
     }else{
-         picSize = pictureNum * 5;
+         picSize = pictureNum * 5; // otherwise it's 5, jpegs are smaller
     }
-    console.log(picSize);
 }else{
-    picSize = pictureNum * 5;
+    picSize = pictureNum * 5; // if it's not a dlsr we just multiply by the known average size of most jpegs
 }
-if(picSize >= cardCap){
+if(picSize >= cardCap){ // if the picsize is at the cap or over we alert the user that he may need to purchase a new card
     result = cardCap - picSize;
     alert("You have " + result + " mbs of memory, Consider replacing your memory card! " );
-}else{
+}else{ // otherwise we just inform them of how much memory they have left.
     result = cardCap - picSize;
     alert("You have " + result + " mbs of memory left");
+    console.log("You have " + result + " mbs of memory left");
 }
 
