02d608b3bef3e1e3dd61a1401662985f2ef6e69d
Vannweb <dawebb@fullsail.edu>
2014-12-18 19:39:05 -0800

    Started temp Program
    started temp program

--- Functions_personal/js/script.js
+++ Functions_personal/js/script.js
@@ -1,2 +1,5 @@
 // Devante Webb 12/18/2014 Functions Personal
 
+alert("Hello this is the temperature regulator. It's our job to keep yu comfortable.");
+var temp = prompt('What is the temperature currently?'  );
+var condition = ('Are you too hot or cold?')
