23de04b72e681b35fce60bada67b52e3e1109692
Vannweb <dawebb@fullsail.edu>
2014-12-18 20:32:21 -0800

    Got rid of static numbers
    Got rid of static numbers for dynamic variable set by user

--- Functions_personal/js/script.js
+++ Functions_personal/js/script.js
@@ -3,13 +3,17 @@
 alert("Hello this is the temperature regulator. It's our job to keep you comfortable.");
 var temp = Number(prompt('What is the temperature currently?'  ));
 var condition = String(prompt('Are you too hot or cold?"Please enter Hot or Cold"'));
-
+var preference = Number(prompt("At what temperature do feel comfortable"));
 if(condition == "Hot"){
-    while (temp > 65){
+    while (temp > preference){
         temp--;
     }
-}if(condition == "Cold")
-
+}if(condition == "Cold"){
+    while (temp < preference){
+        temp++
+    }
+}
+(condition == "Cold") ? alert("Alright! We've set the temperature to " + temp + "\n It'll be warmer soon'") : alert(" Okay! We've set the temperature to " + temp + "\n It'll be cooler shortly'" )
 console.log(temp);
 
 
