726877c109aa9837aba56a09283a1fb2c402ba5d
Vannweb <dawebb@fullsail.edu>
2014-12-18 20:19:52 -0800

    Corrected Problem, Needed a ==
    == keeps catching me off guard

--- Functions_personal/js/script.js
+++ Functions_personal/js/script.js
@@ -4,11 +4,12 @@ alert("Hello this is the temperature regulator. It's our job to keep you comfort
 var temp = Number(prompt('What is the temperature currently?'  ));
 var condition = String(prompt('Are you too hot or cold?"Please enter Hot or Cold"'));
 
-if(condition = "Hot"){
+if(condition == "Hot"){
     while (temp > 65){
         temp--;
     }
-}
+}if(condition == "Cold")
+
 console.log(temp);
 
 
