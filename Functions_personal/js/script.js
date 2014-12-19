b162a3870c977a0029347e18d97ed067d8bbe7d7
Vannweb <dawebb@fullsail.edu>
2014-12-18 20:15:20 -0800

    Console. Log temp for  check

--- Functions_personal/js/script.js
+++ Functions_personal/js/script.js
@@ -1,5 +1,15 @@
 // Devante Webb 12/18/2014 Functions Personal
 
-alert("Hello this is the temperature regulator. It's our job to keep yu comfortable.");
-var temp = prompt('What is the temperature currently?'  );
-var condition = ('Are you too hot or cold?')
+alert("Hello this is the temperature regulator. It's our job to keep you comfortable.");
+var temp = Number(prompt('What is the temperature currently?'  ));
+var condition = String(prompt('Are you too hot or cold?"Please enter Hot or Cold"'));
+
+if(condition = "Hot"){
+    while (temp > 65){
+        temp--;
+    }
+}
+console.log(temp);
+
+
+
